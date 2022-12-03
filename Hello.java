cd6be80edae87e37082cd7c52fef3d34e30d90ea
Vishnusaibore <vishnusaibore830@gmail.com>
2022-12-04 00:55:24 +0530

    This is First Commit

---
+++ Hello.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class Hello{
+    public static void main(String[] args){
+        Scanner s =new Scanner(System.in);
+        String str;
+        System.out.println("Enter any String");
+        str=s.nextLine();
+        System.out.println("The Entered Stirng is:"+str);
+    }
+}
